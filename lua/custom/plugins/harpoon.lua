48b43348165c3fa1aaf71d935d3129fa05cf7754
disonek <disonek@gmail.com>
2024-11-02 14:46:33 +0100

    harpoon keybinding changed

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -22,35 +22,35 @@ return {
       desc = 'harpoon quick menu',
     },
     {
-      '<leader>1',
+      '<leader>h1',
       function()
         require('harpoon'):list():select(1)
       end,
       desc = 'harpoon to file 1',
     },
     {
-      '<leader>2',
+      '<leader>h2',
       function()
         require('harpoon'):list():select(2)
       end,
       desc = 'harpoon to file 2',
     },
     {
-      '<leader>3',
+      '<leader>h3',
       function()
         require('harpoon'):list():select(3)
       end,
       desc = 'harpoon to file 3',
     },
     {
-      '<leader>4',
+      '<leader>h4',
       function()
         require('harpoon'):list():select(4)
       end,
       desc = 'harpoon to file 4',
     },
     {
-      '<leader>5',
+      '<leader>h5',
       function()
         require('harpoon'):list():select(5)
       end,
