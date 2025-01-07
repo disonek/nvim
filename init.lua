035561d3554d27acd70cb9d9f5f84f433f0015cc
GZTKW8W <onufry.pajaczek@siili.com>
2025-01-07 15:29:56 +0100

    Git blame plugin added, error and warnings turned off, copy path added

--- init.lua
+++ init.lua
@@ -192,6 +192,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
+--
+-- turn off warnings/errors
+vim.diagnostic.config { virtual_text = false, signs = false, underline = false }
+
+vim.keymap.set('n', '<leader>p', '<cmd>let @+ = expand("%:p")<CR>')
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
@@ -387,6 +392,11 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
+        defaults = {
+          layout_config = {
+            horizontal = { width = 0.99 },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
@@ -605,7 +615,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        clangd = {
+          settings = {
+            clangd = {},
+          },
+        },
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -932,7 +946,6 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
-  --
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
